33f1f774ea2a4cdb9e0d76a791429e8a6507a983
Ciftelia <klodiana.kamberi87@gmail.com>
2024-11-14 14:43:51 -0500

    Slept on it I think a found a solution to deck.json visibility

--- reader.js
+++ reader.js
@@ -1,4 +1,4 @@
-fetch('FrenchCards/FlashCardsAsJSON/deck.json')
+fetch('https://ciftelia.github.io/FrenchCards/FlashCardsAsJSON/deck.json')
     .then(response => response.json())
     .then(data => {
         data['notes'].forEach(card => {
